6a879de3d2becf6ed3fd7db8d1eda1ffedeef755
Tobias Recker <Recker@match.uni-hannover.de>
2023-12-14 16:43:52 +0100

    add several robots TF publisher by launching

--- my_virtual_leader/scripts/actual_pose_publisher.py
+++ my_virtual_leader/scripts/actual_pose_publisher.py
@@ -15,32 +15,33 @@ from nav_msgs.msg import Odometry
 
 class follower:
     def __init__(self):
-        self.config()
-        rospy.init_node("actual_pose_publishe")
+        rospy.init_node("actual_pose_publisher")
         rospy.loginfo("actual_pose_publiser is running")
+        self.config()
         rospy.Subscriber(self.follower_pose_topic,Pose,self.callback_follower_pose)
         # self.broad_cast()
         rospy.spin()
 
 
     def config(self):
-        self.follower_pose_topic = rospy.get_param('~robot_pose_topic', '/mur620d/mir_pose_simple')
+        self.follower_pose_topic = rospy.get_param('~robot_pose_topic', 'nul')
+        rospy.loginfo(self.follower_pose_topic)
   
 
     def callback_follower_pose(self,data):
         self.pose = Pose()
         self.pose.position = data.position
         self.pose.orientation = data.orientation
-        rospy.loginfo("pose_received")
+        # rospy.loginfo("pose_received")
         br = tf2_ros.TransformBroadcaster()
         t = TransformStamped()
         t.header.stamp = rospy.Time.now()
         t.header.frame_id = "map"
-        t.child_frame_id = "mur_pose"
+        t.child_frame_id = self.follower_pose_topic.split("/")[1]+"/"+"mur_pose"
         t.transform.translation = self.pose.position
         t.transform.rotation = self.pose.orientation
         br.sendTransform(t)
-        rospy.loginfo("running")
+        # rospy.loginfo("running")
 
     def broad_cast(self):   
         pass
